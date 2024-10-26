abab04eb2c636c7366f9ab11ecb37dbbdf5297cd
Arbaz Chaudhary <a4arbazchaudhary@gmail.com>
2024-10-26 12:41:57 +0500

    portfolio

---
+++ data/index.ts
@@ -0,0 +1,185 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Learning to build interactive animations with JavaScript",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Payswift - Transforming Fintech Experiences",
+    des: "An innovative fintech platform that simplifies digital transactions with smooth, user-friendly interfaces, leveraging React, Tailwind, and TypeScript to deliver an efficient financial experience.",
+    img: "/pay.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+  },
+  {
+    id: 2,
+    title: "Pitiklini - Revolutionizing Global Transactions",
+    des: "A full-stack fintech solution integrating PayPal and Stripe, supporting multi-currency transactions. This application enables seamless fund transfers, currency conversion, and external payment links—all built from scratch with React, Express, and MongoDB.",
+    img: "/piti.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+  },
+  {
+    id: 3,
+    title: "Personal Portfolio - Showcase Your Digital Footprint",
+    des: "A dynamic and modern portfolio website featuring AI-powered SaaS solutions, payments integration, and credit systems, built with the latest tech stack to showcase projects and skills effectively.",
+    img: "/port.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+  },
+];
+
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Arbaz on our fintech application was an outstanding experience. His expertise in both frontend and backend development allowed us to create a seamless platform for our users. His attention to detail and proactive approach ensured that we met all our deadlines while maintaining high-quality standards. I highly recommend Arbaz for anyone looking to build innovative financial solutions.",
+    name: "Rafay Nadeem",
+    title: "CEO of FinTech Innovations",
+  },
+];
+
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Contributed to building interactive features for a web-based platform using React.js, enhancing user engagement and experience.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Backend Developer",
+    desc: "Designed and implemented backend systems for web applications, ensuring efficient data handling and secure server-side logic.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Full Stack Developer",
+    desc: "Developed and maintained both frontend and backend components for dynamic web applications, leveraging modern tech stacks to create efficient, responsive features.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 4,
+    title: "Fintech Application Developer",
+    desc: "Led the development of a fintech web application, overseeing the entire process from initial concept to deployment, focusing on seamless transactions and user-friendly interfaces.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+];
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Arbaz128", // GitHub link
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/Muhamma16150616?t=lMRkkYDk2JaEpmxOhhGt6g&s=09", // Twitter link
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/muhammad-arbaz-asif-5316b225b/", // LinkedIn link
+  },
+];
